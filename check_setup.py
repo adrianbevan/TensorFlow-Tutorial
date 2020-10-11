b623d1f67488fdbf2a8da837b413704f1192fd9f
Adrian Bevan <a.j.bevan@qmul.ac.uk>
2020-10-11 15:44:33 +0100

    Updating with sklearn examples for DT, BDT, RF and SVM and having made final checks on the Keras examples for V1.0

--- check_setup.py
+++ check_setup.py
@@ -2,9 +2,11 @@ import matplotlib
 import sklearn
 import tensorflow
 import numpy
+import seaborn
 
 
 print("matplotlib version {:}".format(matplotlib.__version__))
 print("sklearn version {:}".format(sklearn.__version__))
 print("tensorflow version {:}".format(tensorflow.__version__))
 print("numpy version {:}".format(numpy.__version__))
+print("seaborn version {:}".format(seaborn.__version__))
